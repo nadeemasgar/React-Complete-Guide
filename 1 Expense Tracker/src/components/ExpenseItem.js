905647774fce69ab25dfe5ee60dc58a28df128af
Nadeem <nadeemasgar18@gmail.com>
2022-01-31 19:52:16 +0530

    Outputting Dynamic Data & working with expression

--- 1 Expense Tracker/src/components/ExpenseItem.js
+++ 1 Expense Tracker/src/components/ExpenseItem.js
@@ -10,7 +10,7 @@ function ExpenseItem() {
         <div className="expense-item">
             <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description"> 
-                <h2>expenseTitle</h2>
+                <h2>{expenseTitle}</h2>
                 <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
