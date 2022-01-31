643646ecf08970291100750f731736be15f26a79
Nadeem <nadeemasgar18@gmail.com>
2022-01-31 16:18:09 +0530

    Building a First Custom Component

--- 1 Expense Tracker/src/App.js
+++ 1 Expense Tracker/src/App.js
@@ -1,10 +1,16 @@
-function App() {   /* Component */ 
-  return (      /* Desired end state - div, h2, p */
-    <div> 
-      <h2>Let's get started!</h2> 
-      <p> This is also visible! </p>
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  /* Component */
+  return (
+    /* Desired end state - div, h2, p */
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
 
 export default App;
+
+/* A component in react is just a javascript function that return JSX code */
