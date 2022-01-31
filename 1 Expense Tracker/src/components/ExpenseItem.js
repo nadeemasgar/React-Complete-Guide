30ccfbcea6b38aeab5a8f90f79aeda9eb12bde41
Nadeem <nadeemasgar18@gmail.com>
2022-01-31 20:48:09 +0530

    Adding normal JavaScript logic to componnets

--- 1 Expense Tracker/src/components/ExpenseItem.js
+++ 1 Expense Tracker/src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
 
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{month}</div>
+            </div>
             <div className="expense-item__description"> 
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
@@ -19,4 +26,4 @@ export default ExpenseItem;
 
 /* 
   Props allows you to make the components reusable and it allows you to pass data from another componnet to this component
-*/
+*/ 
