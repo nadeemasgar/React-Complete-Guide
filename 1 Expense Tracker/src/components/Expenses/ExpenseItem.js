294e7b4a264b92540d7dae9170ce9a0c6aae1914
Nadeem <nadeemasgar18@gmail.com>
2022-02-02 00:08:14 +0530

    Example to add event Listner to react element

--- 1 Expense Tracker/src/components/Expenses/ExpenseItem.js
+++ 1 Expense Tracker/src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,29 @@
 import React from "react";
-import ExpenseDate from './ExpenseDate';
+import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    return (
-        <Card className="expense-item">
-            <ExpenseDate  date={props.date} />
-            <div className="expense-item__description"> 
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            <button onClick={ () => {console.log('Clicked')}}>Change Title</button>
-        </Card>
-    );
-}
+  const clickHandler = () => {
+    console.log("Clicked!!!!");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
 
-/* Use className instead of class */ 
+/* Use className instead of class */
 
 /* 
   Props allows you to make the components reusable and it allows you to pass data from another componnet to this component
-*/ 
+*/
