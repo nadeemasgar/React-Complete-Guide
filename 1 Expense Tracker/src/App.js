b6c2ab448cfe4865442451144e9add3b665ff5d3
Nadeem <nadeemasgar18@gmail.com>
2022-02-01 18:28:18 +0530

    Changes reverted back

--- 1 Expense Tracker/src/App.js
+++ 1 Expense Tracker/src/App.js
@@ -25,19 +25,12 @@ function App() {
     },
   ];
 
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Let's get started!"),
-    React.createElement(Expenses, { items: expenses })
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
   );
-
-  // return (
-  //   <div>
-  //     <h2>Let's get started!</h2>
-  //     <Expenses items={expenses} />
-  //   </div>
-  // );
 }
 
 export default App;
