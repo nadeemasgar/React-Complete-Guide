c26d277c8f1ba8a18c46910015143c9d1e399c6a
Nadeem <nadeemasgar18@gmail.com>
2022-02-04 18:58:03 +0530

    Two-Way Bind to controll default value: Assignment

--- 1 Expense Tracker/src/components/Expenses/Expenses.js
+++ 1 Expense Tracker/src/components/Expenses/Expenses.js
@@ -14,7 +14,7 @@ const Expenses = (props) => {
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter onchageFilter={filterChangeHandler} />
+        <ExpensesFilter selected={filteredYear} onchageFilter={filterChangeHandler} />
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
