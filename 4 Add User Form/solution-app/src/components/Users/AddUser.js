d4366abfbde3ee24837b7525a7502e1952ac2757
Nadeem <nadeemasgar18@gmail.com>
2022-02-10 15:44:23 +0530

    Creating a Wrapper Component

--- 4 Add User Form/solution-app/src/components/Users/AddUser.js
+++ 4 Add User Form/solution-app/src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
@@ -49,7 +50,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -78,7 +79,7 @@ const AddUser = (props) => {
           </Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
