d6015f2835694060ee64d84dab5eac1d4cabacd4
Nadeem <nadeemasgar18@gmail.com>
2022-02-11 14:57:57 +0530

    This code runs only once when the app restart

--- 5 SideEffects-Reduces-Context API/Login-Logout/src/App.js
+++ 5 SideEffects-Reduces-Context API/Login-Logout/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -6,20 +6,24 @@ import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
 
-  if (storedUserLoggedInInformation === "1") {
-    setIsLoggedIn(true);
-  }
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  /* function here would indeed only run once when the app starts because thereafter the dependencies never change */
+  /* This data fetching is a side effect. It's not directly related to the UI. */
 
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
     localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
