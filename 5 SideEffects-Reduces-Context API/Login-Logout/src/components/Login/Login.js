b39f90f87ec3d740c73084b6e917dbf8a134f639
Nadeem <nadeemasgar18@gmail.com>
2022-02-11 16:30:45 +0530

    useEffect Cleanup function added

--- 5 SideEffects-Reduces-Context API/Login-Logout/src/components/Login/Login.js
+++ 5 SideEffects-Reduces-Context API/Login-Logout/src/components/Login/Login.js
@@ -12,12 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => {
+    const identifier = setTimeout(() => {
       console.log("Checking from validity!");
       setFormIsValid(
         enteredEmail.includes("@") && enteredPassword.trim().length > 6
       );
     }, 500);
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
