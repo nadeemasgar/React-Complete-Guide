00bb9ac6f8fe3f92630499a5c5722d7f2a6bceb8
Nadeem <nadeemasgar18@gmail.com>
2022-02-13 23:53:26 +0530

    Forward Refs & useImperativeHandle

--- 5 SideEffects-Reduces-Context API/use Reducer project/src/components/Login/Login.js
+++ 5 SideEffects-Reduces-Context API/use Reducer project/src/components/Login/Login.js
@@ -112,9 +112,9 @@ const Login = (props) => {
     if (formIsValid) {
       authCtx.onLogin(emailState.value, passwordState.value);
     } else if (!emailIsValid) {
-      emailInputRef.current.activate();
+      emailInputRef.current.focus();
     } else {
-      passwordInputRef.current.activate();
+      passwordInputRef.current.focus();
     }
   };
 
