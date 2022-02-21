2e08a128b9d0a0d5a8339cfbfbf6899bfbd83076
Nadeem <nadeemasgar18@gmail.com>
2022-02-21 16:04:34 +0530

    2nd way of Adjusting Custom Hook Logic commited

--- 11 Custom Hooks/2 Http Logic/src/App.js
+++ 11 Custom Hooks/2 Http Logic/src/App.js
@@ -7,23 +7,25 @@ import useHttp from "./hooks/use-http";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  
-
   const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-
     const transformTasks = (tasksObj) => {
       const loadedTasks = [];
-  
+
       for (const taskKey in tasksObj) {
         loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
       }
-  
+
       setTasks(loadedTasks);
     };
 
-    fetchTasks({ url: "https://react-app-1cfa3-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",}, transformTasks);
+    fetchTasks(
+      {
+        url: "https://react-app-1cfa3-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+      },
+      transformTasks
+    );
   }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
