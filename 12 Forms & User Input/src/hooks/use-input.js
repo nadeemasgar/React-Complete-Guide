b825e9f91adad17de56222d137005ee2801bbecc
Nadeem <nadeemasgar18@gmail.com>
2022-02-23 12:15:11 +0530

    Using useReducer (replacing multiple useState)

--- 12 Forms & User Input/src/hooks/use-input.js
+++ 12 Forms & User Input/src/hooks/use-input.js
@@ -1,32 +1,55 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  // Parameter => previous state, action to dispatch in your code
+  if (action.type === "INPUT") {
+    // return new state snapshot
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { value: state.value, isTouched: true };
+  }
+  if (action.type === "RESET") {
+    return { value: "", isTouched: false };
+  }
+  return inputStateReducer;
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    // parameter -> reducer-Function, initialState
+    // Return an array -> inputState, dispatch function which allows you to dispatch action against that reducer
+    inputStateReducer,
+    initialInputState
+  );
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value); 
+    dispatch({ type: "INPUT", value: event.target.value });
   };
 
   const inputBlurHandler = (event) => {
-    setIsTouched(true);
+    dispatch({ type: "BLUR" });
   };
 
   const reset = () => {
-      setEnteredValue("");
-      setIsTouched(false);
-  }
+    dispatch({ type: "RESET" });
+  };
 
   return {
-    value: enteredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
     inputBlurHandler,
-    reset
+    reset,
   };
 };
 
