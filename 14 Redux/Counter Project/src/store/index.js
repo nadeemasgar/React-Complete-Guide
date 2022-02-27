4bf97904caf23feaa733e8995da77de6412f9a78
Nadeem <nadeemasgar18@gmail.com>
2022-02-27 20:46:15 +0530

    Never change existing state. Create brand new obj

--- 14 Redux/Counter Project/src/store/index.js
+++ 14 Redux/Counter Project/src/store/index.js
@@ -5,6 +5,10 @@ const initialState = { counter: 0, showCounter: true };
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
+      // We must always set all the other states when we update a piece of state because we overwrite the old state.
+      // You should never change the existing state. Instead, always override it by returning a brand new state object.
+      /* You must never mutate state that is never changed the original state which you're getting.
+      This can lead to bugs, unpredictable behavior. Instead create a brand new state and override the original state */
       counter: state.counter + 1,
       showCounter: state.showCounter,
     };
