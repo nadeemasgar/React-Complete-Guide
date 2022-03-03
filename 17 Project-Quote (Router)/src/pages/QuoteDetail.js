25666e9a9363a44115d3bb6dc6d21e9023e5e14c
Nadeem <nadeemasgar18@gmail.com>
2022-03-03 13:38:11 +0530

    Adding Nested Routes with Dynamic Route (path/URL)

--- 17 Project-Quote (Router)/src/pages/QuoteDetail.js
+++ 17 Project-Quote (Router)/src/pages/QuoteDetail.js
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
   const params = useParams();
@@ -8,8 +10,17 @@ const QuoteDetail = () => {
     <Fragment>
       <h1>Quote Detail Page</h1>
       <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
 
 export default QuoteDetail;
+
+/* 
+    <Route path="/quotes/:quoteId/comments"> // Another way of adding Dynamic Path
+        <Comments />
+    </Route>
+*/
