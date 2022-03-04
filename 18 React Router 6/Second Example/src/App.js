7b4a492d676d6dac9cda607db5d3f870d2f0e6c3
Nadeem <nadeemasgar18@gmail.com>
2022-03-05 00:30:46 +0530

    Navigate, Nested routes, Relative path needed

--- 18 React Router 6/Second Example/src/App.js
+++ 18 React Router 6/Second Example/src/App.js
@@ -1,29 +1,23 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Routes, Navigate } from "react-router-dom";
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import MainHeader from './components/MainHeader';
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Switch>
-          <Route path='/' exact>
-            <Redirect to='/welcome' />
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/welcome" />} />
+          <Route path="/welcome/*" element={<Welcome />}>
+            <Route path="new-user" element={<p>Welcome, new user!</p>}></Route>
           </Route>
-          <Route path='/welcome'>
-            <Welcome />
-          </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
+        </Routes>
       </main>
     </div>
   );
@@ -31,6 +25,9 @@ function App() {
 
 export default App;
 
-// our-domain.com/welcome => Welcome Component
-// our-domain.com/products => Products Component
-// our-domain.com/product-detail/a-book
+// Navigate component is the replacement of Redirect component
+// element={<Navigate to="/welcome" />}  => It will push a navigation to this page onto the navigation stack.
+/* element={<Navigate replace to="/welcome" />}  => If we truly want to redirect, so replace the current page with the new page,
+   then we would have to add the replace prop to Navigate as well.
+*/
+
--- 18 React Router 6/Second Example/src/pages/Welcome.js
+++ 18 React Router 6/Second Example/src/pages/Welcome.js
@@ -1,14 +1,32 @@
-import { Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
-      <Route path="/welcome/new-user">
-        <p>Welcome, new user!</p>
-      </Route>
+      <Routes>
+        <Route path="new-user" element={<p>Welcome, new user!</p>} />
+      </Routes>
     </section>
   );
 };
 
 export default Welcome;
+
+/* 
+  Nested routes can be useful because they allow you to build deeply nested page structures
+
+  We don't have to wrap the Route definition components with the Routes or Switch component.
+  With version 6, it is mandatory to wrap using Routes.
+*/
+
+// The paths of these nested routes actually are relative to the path of the parent route.
+
+/* 
+  With version 6, since this is a link inside a component that already is loaded through a route, should have relative path.
+  So links and routes are relative if they are loaded inside of a component that is already loaded because of another route.
+  So if you're in a nested route, so to say.
+          <Link className="btn--flat" to={`${match.url}/comments`}>Load Comments</Link>
+          <Link  to={`${match.path}/comments`}><Comments /></Link>
+  So, full path not needed in v-6. Instead use relative path
+*/
