5b637e39306809007beb0bc4bdaf3d4cbf5094fc
Nadeem <nadeemasgar18@gmail.com>
2022-03-09 00:04:08 +0530

    Optimizing fetch req using setTimeout, return line

--- 22 Summary React Hooks/Hooks Project/src/components/Ingredients/Search.js
+++ 22 Summary React Hooks/Hooks Project/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,28 +6,38 @@ import "./Search.css";
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props; // Object Destructuring
   const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch(
-      `https://react-app-1cfa3-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json${query}`
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    // After 500 milisecond, when the user stop typing which means value entered before 500 milisecond should be equal to current value,
+    // then we have to send request to the server
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          `https://react-app-1cfa3-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json${query}`
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -35,6 +45,7 @@ const Search = React.memo((props) => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={(event) => setEnteredFilter(event.target.value)}
