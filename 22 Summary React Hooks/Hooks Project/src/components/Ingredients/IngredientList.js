e2c7fd4e06bc87d53845c7e53bb936d73a459255
Nadeem <nadeemasgar18@gmail.com>
2022-03-08 13:49:46 +0530

    Bind Related Code

--- 22 Summary React Hooks/Hooks Project/src/components/Ingredients/IngredientList.js
+++ 22 Summary React Hooks/Hooks Project/src/components/Ingredients/IngredientList.js
@@ -8,7 +8,7 @@ const IngredientList = props => {
       <h2>Loaded Ingredients</h2>
       <ul>
         {props.ingredients.map(ig => (
-          <li key={ig.id} onClick={(event) => props.onRemoveItem(event, ig)}>
+          <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
             <span>{ig.title}</span>
             <span>{ig.amount}x</span>
           </li>
